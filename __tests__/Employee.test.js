ae22e8abafccfc61591c48b05fe8e768a378747d
Mark <markloercher@gmail.com>
2021-12-10 12:15:38 -0500

    tested Employee class

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,44 @@
+const Employee = require("../lib/Employee");
+
+test ("instantiate Employee object", () => {
+    var employee1 = new Employee ("Mark", "123", "Mark@email.com")
+    expect(typeof(employee1)).toBe("object")
+})
+
+test ("setting a name", () => {
+    var employee1 = new Employee ("Mark", "123", "Mark@email.com")
+    expect(employee1.name).toBe("Mark")
+})
+
+test ("setting an id", () => {
+    var employee1 = new Employee ("Mark", "123", "Mark@email.com")
+    expect(employee1.id).toBe("123")
+})
+
+test ("setting an email", () => {
+    var employee1 = new Employee ("Mark", "123", "Mark@email.com")
+    expect(employee1.email).toBe("Mark@email.com")
+})
+
+//testing methods 
+test ("getting a name", () => {
+    var employee1 = new Employee ("Mark", "123", "Mark@email.com")
+    expect(employee1.getName()).toBe("Mark")
+})
+
+test ("getting an id", () => {
+    var employee1 = new Employee ("Mark", "123", "Mark@email.com")
+    expect(employee1.getId()).toBe("123")
+})
+
+test ("getting an email", () => {
+    var employee1 = new Employee ("Mark", "123", "Mark@email.com")
+    expect(employee1.getEmail()).toBe("Mark@email.com")
+})
+
+//getRole() test
+test ("getting an role", () => {
+    var employee1 = new Employee ("Mark", "123", "Mark@email.com")
+    expect(employee1.getRole()).toBe("Employee")
+})
+
